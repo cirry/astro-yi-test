d474f19485bd8247bc522a1c9e53c6382c4c310e
cirry <>
2024-09-19 17:11:54 +0800

    fix: test

--- src/utils/getUrl.ts
+++ src/utils/getUrl.ts
@@ -3,12 +3,12 @@ import {config} from '../consts'
 
 /**
  * handle GitHub Pages url or other prefix url
- * @param suffixUrl
+ * @param suffixUrl blog
  * @result /astro-yi/blog
  */
 export const getUrl = (suffixUrl ?: string): string => {
   if (!isProd) {
     return ''
   }
-  return config.githubPagePrefix + suffixUrl
+  return '/' + config.githubPagePrefix + '/' + suffixUrl
 }
